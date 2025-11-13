acb2d2b53e61fb00f98ec7d23a451e7318064f25
alex vikol <sashavikol7@gmail.com>
2025-11-13 22:13:48 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.VikolAS.Sprint6.Task0.V24.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.VikolAS.Sprint6.Task0.V24.Lib
+{
+    public class DataService : ISprint6Task0V24
+    {
+        public double Calculate(int x)
+        {
+            double y = Math.Pow((x - 1), (3 * x + 1));
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.VikolAS.Sprint6.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.VikolAS.Sprint6.Task0.V24.Lib;
+
+namespace Tyuiu.VikolAS.Sprint6.Task0.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 4096; // (3 - 1)^(3*3+1) = 2^10 = 1024
+            Assert.AreEqual(1024, res);
+        }
+    }
+}
